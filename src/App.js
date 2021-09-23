ddf0c25f9f36fa768ed7c8e40af12b2b0de01996
Olena40 <o.redchuk@gmx.de>
2021-09-23 18:12:31 +0200

    added checkbox

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 
 
 import { useState } from 'react';
-import Input from './Components/Input'
+import Input from './Components/Input';
+
 
 function App() {
 
@@ -11,27 +12,39 @@ function App() {
   const [value, setValue] = useState("");
   const [todos, setTodos] = useState([]);
 
+
   function handleInput(e) {
     setValue(e.target.value);
-    
   }
    
 
   function handleSubmit(e) {
-    e.preventDefault();
+    e.preventDefault(); 
     setTodos([...todos, value]);
-    
+setValue ('');
   }
   
 
   return (
     <div className="App">
-      <p>Test</p>
-      
-      <Input handleInput={handleInput} handleSubmit={handleSubmit} />
+      <p>ToDo</p>
+     
+      <form >
+       
+      <Input handleInput={handleInput} handleSubmit={handleSubmit} value={value} />
       <ul className="TodoListe">
-        {todos && todos.map((todo, index) => <li key={index}>{todo}</li>)}
+        {todos && todos.map((todo, index) => <li key={index} className='li'>
+        <span >
+          <input 
+          type='checkbox' />
+             &nbsp; 
+      {todo} 
+      &nbsp;
+      </span>
+      <button className='rm' >&times;</button>
+          </li>)}
       </ul>
+      </form>
     </div>
   );
 }
--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,11 +1,15 @@
-import React from "react";
 
-function Input({ handleInput, handleSubmit }) {
+import React from 'react'
+
+
+function Input({ handleInput, handleSubmit, value }) {
   return (
     <form >
-      <input type="text" onChange={handleInput} />
+      <input type="text" value = {value} onChange={handleInput} />
       <input type="submit" onClick={handleSubmit} />
     </form>
   );
 }
 export default Input;
+
+
